fcc3c9f2855ed1606aa2cf28ae52a52485c6f220
Lindsey Bradford <lindseyb@wearetelegraph.com>
2019-01-19 12:45:45 -0600

    + site header styles + site footer styles + icons

--- tailwind.js
+++ tailwind.js
@@ -46,7 +46,7 @@ View the full documentation at https://tailwindcss.com.
 let colors = {
     'transparent': 'transparent',
   
-    'black': '#22292f',
+    'black': '#000000',
     'grey-darkest': '#3d4852',
     'grey-darker': '#606f7b',
     'grey-dark': '#8795a1',
@@ -55,78 +55,81 @@ let colors = {
     'grey-lighter': '#f1f5f8',
     'grey-lightest': '#f1f1f1',
     'white': '#ffffff',
+
+    'oat': '#EDCCA5',
+    'oat-light': '#F1ECE4',
   
-    'red-darkest': '#3b0d0c',
-    'red-darker': '#621b18',
-    'red-dark': '#cc1f1a',
-    'red': '#e3342f',
-    'red-light': '#ef5753',
-    'red-lighter': '#f9acaa',
-    'red-lightest': '#fcebea',
+    // 'red-darkest': '#3b0d0c',
+    // 'red-darker': '#621b18',
+    // 'red-dark': '#cc1f1a',
+    // 'red': '#e3342f',
+    // 'red-light': '#ef5753',
+    // 'red-lighter': '#f9acaa',
+    // 'red-lightest': '#fcebea',
   
-    'orange-darkest': '#462a16',
-    'orange-darker': '#613b1f',
-    'orange-dark': '#de751f',
-    'orange': '#f6993f',
-    'orange-light': '#faad63',
-    'orange-lighter': '#fcd9b6',
-    'orange-lightest': '#fff5eb',
+    // 'orange-darkest': '#462a16',
+    // 'orange-darker': '#613b1f',
+    // 'orange-dark': '#de751f',
+    'orange': '#E8A879',
+    // 'orange-light': '#faad63',
+    // 'orange-lighter': '#fcd9b6',
+    // 'orange-lightest': '#fff5eb',
   
-    'yellow-darkest': '#453411',
-    'yellow-darker': '#684f1d',
-    'yellow-dark': '#f2d024',
-    'yellow': '#ffed4a',
-    'yellow-light': '#fff382',
-    'yellow-lighter': '#fff9c2',
-    'yellow-lightest': '#fcfbeb',
+    // 'yellow-darkest': '#453411',
+    // 'yellow-darker': '#684f1d',
+    // 'yellow-dark': '#f2d024',
+    // 'yellow': '#ffed4a',
+    // 'yellow-light': '#fff382',
+    // 'yellow-lighter': '#fff9c2',
+    // 'yellow-lightest': '#fcfbeb',
   
-    'green-darkest': '#0f2f21',
-    'green-darker': '#1a4731',
-    'green-dark': '#1f9d55',
-    'green': '#38c172',
-    'green-light': '#51d88a',
-    'green-lighter': '#a2f5bf',
-    'green-lightest': '#e3fcec',
+    'green-darkest': '#12302D',
+    // 'green-darker': '#1a4731',
+    // 'green-dark': '#1f9d55',
+    'green': '#53C07D',
+    'green-light': '#A0E0B2',
+    // 'green-lighter': '#a2f5bf',
+    // 'green-lightest': '#e3fcec',
   
-    'teal-darkest': '#0d3331',
-    'teal-darker': '#20504f',
-    'teal-dark': '#38a89d',
-    'teal': '#4dc0b5',
-    'teal-light': '#64d5ca',
-    'teal-lighter': '#a0f0ed',
-    'teal-lightest': '#e8fffe',
+    // 'teal-darkest': '#0d3331',
+    // 'teal-darker': '#20504f',
+    // 'teal-dark': '#38a89d',
+    // 'teal': '#4dc0b5',
+    // 'teal-light': '#64d5ca',
+    // 'teal-lighter': '#a0f0ed',
+    // 'teal-lightest': '#e8fffe',
   
-    'blue-darkest': '#12283a',
-    'blue-darker': '#1c3d5a',
-    'blue-dark': '#2779bd',
-    'blue': '#3490dc',
-    'blue-light': '#6cb2eb',
-    'blue-lighter': '#bcdefa',
-    'blue-lightest': '#eff8ff',
+    // 'blue-darkest': '#12283a',
+    // 'blue-darker': '#1c3d5a',
+    // 'blue-dark': '#2779bd',
+    // 'blue': '#3490dc',
+    // 'blue-light': '#6cb2eb',
+    // 'blue-lighter': '#bcdefa',
+    // 'blue-lightest': '#eff8ff',
   
-    'indigo-darkest': '#191e38',
-    'indigo-darker': '#2f365f',
-    'indigo-dark': '#5661b3',
-    'indigo': '#6574cd',
-    'indigo-light': '#7886d7',
-    'indigo-lighter': '#b2b7ff',
-    'indigo-lightest': '#e6e8ff',
+    // 'indigo-darkest': '#191e38',
+    // 'indigo-darker': '#2f365f',
+    // 'indigo-dark': '#5661b3',
+    // 'indigo': '#6574cd',
+    // 'indigo-light': '#7886d7',
+    // 'indigo-lighter': '#b2b7ff',
+    // 'indigo-lightest': '#e6e8ff',
   
-    'purple-darkest': '#21183c',
-    'purple-darker': '#382b5f',
-    'purple-dark': '#794acf',
-    'purple': '#9561e2',
-    'purple-light': '#a779e9',
-    'purple-lighter': '#d6bbfc',
-    'purple-lightest': '#f3ebff',
+    // 'purple-darkest': '#21183c',
+    // 'purple-darker': '#382b5f',
+    // 'purple-dark': '#794acf',
+    // 'purple': '#9561e2',
+    // 'purple-light': '#a779e9',
+    // 'purple-lighter': '#d6bbfc',
+    // 'purple-lightest': '#f3ebff',
   
-    'pink-darkest': '#451225',
-    'pink-darker': '#6f213f',
-    'pink-dark': '#eb5286',
-    'pink': '#f66d9b',
-    'pink-light': '#fa7ea8',
-    'pink-lighter': '#ffbbca',
-    'pink-lightest': '#ffebef',
+    // 'pink-darkest': '#451225',
+    // 'pink-darker': '#6f213f',
+    // 'pink-dark': '#eb5286',
+    // 'pink': '#f66d9b',
+    // 'pink-light': '#fa7ea8',
+    // 'pink-lighter': '#ffbbca',
+    // 'pink-lightest': '#ffebef',
   }
   
   module.exports = {
@@ -234,7 +237,7 @@ let colors = {
       'sm': '.875rem',    // 14px
       'base': '1rem',     // 16px
       'lg': '1.125rem',   // 18px
-      'xl': '1.25rem',    // 20px
+      'xl': '1.25rem',    // 22px
       '2xl': '1.5rem',    // 24px
       '3xl': '1.875rem',  // 30px
       '4xl': '2.25rem',   // 36px
@@ -634,6 +637,7 @@ let colors = {
       '8': '2rem',
       '10': '2.5rem',
       '12': '3rem',
+      '14': '3.75rem', // 60px
       '16': '4rem',
       '20': '5rem',
       '24': '6rem',
@@ -672,6 +676,7 @@ let colors = {
       '16': '4rem',
       '20': '5rem',
       '24': '6rem',
+      '30': '7.5rem',
       '32': '8rem',
     },
   
@@ -905,12 +910,12 @@ let colors = {
     |
     */
   
-    plugins: [
-      require('tailwindcss/plugins/container')({
-        center: true,
-        padding: '1rem',
-      }),
-    ],
+    // plugins: [
+    //   require('tailwindcss/plugins/container')({
+    //     center: true,
+    //     padding: '1rem',
+    //   }),
+    // ],
   
   
     /*
